ad9214cb3600a9ddfa2d53fcbe4764e504ce029c
clark-lindsay <clarklindsay96@gmail.com>
2020-02-24 17:42:08 -0500

    created a randomCreature function that compiles all types of creatures, and returns one at random, with the option to specify a challenge rating

--- randomCreature.test.ts
+++ randomCreature.test.ts
@@ -1,17 +1,37 @@
-import { randomBeast } from './randomCreature';
+import { randomBeast, randomCreature } from './randomCreature';
 
 describe('the randomBeast function', () => {
-  it('will return an object that matches the Creature interface when called with no arguments', () => {
+  it('will return an object that matches the Creature interface, and has type "Beast", when called with no arguments', () => {
     const creature = randomBeast();
+    expect(typeof creature.name).toEqual('string');
+    expect(typeof creature.challengeRating).toEqual('number');
+    expect(Array.isArray(creature.terrains)).toBeTruthy();
+    expect(creature.type).toEqual('Beast');
+  });
+
+  it('will return only beasts whose challenge rating matches the rating given', () => {
+    for (let i = 0; i < 3; ++i) {
+      expect(randomBeast(0).challengeRating).toEqual(0);
+    }
+  });
+});
+
+describe('the randomCreature function', () => {
+  it('will return an object that matches the Creature interface when called with no arguments', () => {
+    const creature = randomCreature();
     expect(typeof creature.name).toEqual('string');
     expect(typeof creature.challengeRating).toEqual('number');
     expect(Array.isArray(creature.terrains)).toBeTruthy();
     expect(typeof creature.type).toEqual('string');
   });
 
-  it('will return only beasts whose challenge rating matches the rating given', () => {
+  it('will return only creatures whose challenge rating matches the rating given', () => {
     for (let i = 0; i < 3; ++i) {
-      expect(randomBeast(0).challengeRating).toEqual(0);
+      expect(randomCreature(0).challengeRating).toEqual(0);
+    }
+
+    for (let i = 0; i < 3; ++i) {
+      expect(randomCreature(1).challengeRating).toEqual(1);
     }
   });
 });
--- randomCreature.ts
+++ randomCreature.ts
@@ -2,6 +2,14 @@ import { Creature } from './Creature';
 import { creatures } from './creatures';
 import { randomInt } from './util';
 
+export function randomCreature(challengeRating?: number): Creature {
+  let result = Object.values(creatures).reduce((accumulator, subList) => accumulator.concat(subList), []);
+  if (typeof challengeRating !== 'undefined') {
+    result = result.filter(creature => creature.challengeRating === challengeRating);
+  }
+  return result[randomInt(0, result.length)];
+}
+
 export function randomBeast(challengeRating?: number): Creature {
   let beasts = creatures.beasts;
   if (typeof challengeRating !== 'undefined') {
