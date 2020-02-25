8e26b2107fe439c5e625fa851627c145e3363d82
clark-lindsay <clarklindsay96@gmail.com>
2020-02-24 19:43:09 -0500

    changed the interface arguments again for getting a random creature so that one can define a min and max challenge rating

--- randomCreature.ts
+++ randomCreature.ts
@@ -1,11 +1,15 @@
 import { Creature } from './Creature';
 import { creatures } from './creatures';
 import { randomInt } from './util';
+import { isNullOrUndefined } from 'util';
 
-export function randomCreature({ challengeRating, types }: randomCreatureArgs): Creature {
+export function randomCreatures({ challengeRatingMin, challengeRatingMax, types }: randomCreaturesArgs): Creature {
   let result = Object.values(creatures).reduce((accumulator, subList) => accumulator.concat(subList), []);
-  if (typeof challengeRating !== 'undefined') {
-    result = result.filter(creature => creature.challengeRating === challengeRating);
+  if (!isNullOrUndefined(challengeRatingMin)) {
+    result = result.filter(creature => creature.challengeRating >= challengeRatingMin);
+  }
+  if (!isNullOrUndefined(challengeRatingMax)) {
+    result = result.filter(creature => creature.challengeRating <= challengeRatingMax);
   }
   if (types) {
     result = result.filter(creature => types.includes(creature.type));
@@ -13,7 +17,9 @@ export function randomCreature({ challengeRating, types }: randomCreatureArgs): 
   return result[randomInt(0, result.length)];
 }
 
-interface randomCreatureArgs {
-  challengeRating?: number;
+interface randomCreaturesArgs {
+  challengeRatingMin?: number;
+  challengeRatingMax?: number;
   types?: string[];
+  count?: number;
 }
