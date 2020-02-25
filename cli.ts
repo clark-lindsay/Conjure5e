1faa10313c2282d87d81b8075fa844c2a3edfbb0
clark-lindsay <clarklindsay96@gmail.com>
2020-02-25 18:35:08 -0500

    Rudimentary logic for prompting the user for a challenge rating for their spell, and returning a list of matching creatures. Tested only manually.

--- cli.ts
+++ cli.ts
@@ -1,13 +1,16 @@
 import arg from 'arg';
 import inquirer from 'inquirer';
+import { randomCreatures } from './randomCreatures';
 
 export async function cli(args: any) {
   let options = parseArgumentsIntoOptions(args);
-  options = await promptForMissingOptions(options);
-  console.log(options);
+  options = await promptForSpellChoice(options);
+  options = await promptForSpellParameters(options);
+  const creatures = randomCreatures({ challengeRatingMin: options.challengeRating, count: 5 });
+  console.log(creatures);
 }
 
-function parseArgumentsIntoOptions(rawArgs: string[]): any {
+function parseArgumentsIntoOptions(rawArgs: string[]): Options {
   const args = arg(
     {
       '--any': Boolean,
@@ -24,13 +27,12 @@ function parseArgumentsIntoOptions(rawArgs: string[]): any {
   };
 }
 
-async function promptForMissingOptions(options: any) {
+async function promptForSpellChoice(options: Options): Promise<Options> {
   if (options.any) {
     return { ...options };
   }
 
   const defaultSpell = 'Conjure Animals';
-
   const questions = [];
   if (!options.spell) {
     questions.push({
@@ -45,6 +47,33 @@ async function promptForMissingOptions(options: any) {
   const answers = await inquirer.prompt(questions);
   return {
     ...options,
-    spell: options.spell || answers.spell
+    spell: options.spell || String(answers.spell)
   };
+}
+
+async function promptForSpellParameters(options: any): Promise<Options> {
+  if (options.any) {
+    return { ...options };
+  }
+
+  const questions = [];
+  questions.push({
+    type: 'list',
+    name: 'challengeRating',
+    message: 'Please choose what challenge rating you would like the summoned creatures to be:',
+    choices: [0, 0.125, 0.25, 0.5, 1, 2], //TODO: choices need to be spell dependent
+    default: 1
+  });
+
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...options,
+    challengeRating: answers.challengeRating
+  };
+}
+
+interface Options {
+  any?: boolean;
+  spell?: string;
+  challengeRating?: number;
 }
