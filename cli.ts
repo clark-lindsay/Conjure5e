7c6b7283e763e4d031434e5490ae7a5d9134f97a
clark-lindsay <clarklindsay96@gmail.com>
2020-02-25 18:08:29 -0500

    Built an option interface so that one can pick a spell interactively

--- cli.ts
+++ cli.ts
@@ -1,7 +1,9 @@
 import arg from 'arg';
+import inquirer from 'inquirer';
 
-export function cli(args: any): void {
-  const options = parseArgumentsIntoOptions(args);
+export async function cli(args: any) {
+  let options = parseArgumentsIntoOptions(args);
+  options = await promptForMissingOptions(options);
   console.log(options);
 }
 
@@ -18,6 +20,31 @@ function parseArgumentsIntoOptions(rawArgs: string[]): any {
 
   return {
     any: args['--any'] || false,
-    template: args._[0]
+    spell: args._[0]
+  };
+}
+
+async function promptForMissingOptions(options: any) {
+  if (options.any) {
+    return { ...options };
+  }
+
+  const defaultSpell = 'Conjure Animals';
+
+  const questions = [];
+  if (!options.spell) {
+    questions.push({
+      type: 'list',
+      name: 'spell',
+      message: 'Please choose which spell you would like to cast.',
+      choices: ['Conjure Animals', 'Conjure Woodland Beings'],
+      default: defaultSpell
+    });
+  }
+
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...options,
+    spell: options.spell || answers.spell
   };
 }
