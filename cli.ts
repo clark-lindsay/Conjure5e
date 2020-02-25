11910ef5f7bf3df07aab919ee111f023549313b5
clark-lindsay <clarklindsay96@gmail.com>
2020-02-25 17:52:34 -0500

    Put in the first command line option and tested manually for correct output in the terminal

--- cli.ts
+++ cli.ts
@@ -1,3 +1,23 @@
+import arg from 'arg';
+
 export function cli(args: any): void {
-  console.log(args);
+  const options = parseArgumentsIntoOptions(args);
+  console.log(options);
+}
+
+function parseArgumentsIntoOptions(rawArgs: string[]): any {
+  const args = arg(
+    {
+      '--any': Boolean,
+      '-a': '--any'
+    },
+    {
+      argv: rawArgs.slice(2)
+    }
+  );
+
+  return {
+    any: args['--any'] || false,
+    template: args._[0]
+  };
 }
