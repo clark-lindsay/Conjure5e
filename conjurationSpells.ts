2a4cd666c28f7bf2e212142eb1c56cb85248b7c7
clark-lindsay <clarklindsay96@gmail.com>
2020-02-26 12:56:35 -0500

    Added a type parameter to the randomCreatures call to make sure that only beasts are summoned, as specified by the spell

--- conjurationSpells.ts
+++ conjurationSpells.ts
@@ -4,7 +4,8 @@ import { Creature } from './Creature';
 export function conjureAnimals(parameters: ConjurationArguments): Creature[] {
   return randomCreatures({
     challengeRatingMin: parameters.challengeRating,
-    challengeRatingMax: parameters.challengeRating
+    challengeRatingMax: parameters.challengeRating,
+    types: ['Beast']
   });
 }
 
