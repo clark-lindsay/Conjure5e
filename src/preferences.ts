eab15566eda27013ba4e4420696582d6964bbffb
clark-lindsay <clarklindsay96@gmail.com>
2020-07-27 12:56:18 -0500

    new function to record the last spell used
    writes the arguments and name of the spell used to a config
    file, with an optional path parameter

--- src/preferences.ts
+++ src/preferences.ts
@@ -1,10 +1,29 @@
-import { writeFileSync } from 'fs';
+import { writeFileSync, PathLike } from 'fs';
 
-export function writeSourcePreferences(sources: string[], destinationFolder = ''): void {
+export function writeSourcePreferences(sources: string[], destinationFolder: PathLike = ''): void {
   const data = {
     sources: sources
   };
-  const destination = destinationFolder == '' ? 'preferredSources.json' : `${destinationFolder}/preferredSources.json`;
+  const destination = destinationFolder === '' ? 'preferredSources.json' : `${destinationFolder}/preferredSources.json`;
+
+  writeFileSync(destination, JSON.stringify(data));
+}
+
+export function recordArguments(
+  spell: string,
+  challengeRating: number,
+  terrains: string[],
+  sources: string[],
+  destinationFolder: PathLike = ''
+): void {
+  const data = {
+    spell: spell,
+    challengeRating: challengeRating,
+    terrains: terrains,
+    sources: sources
+  };
+
+  const destination = destinationFolder ? `${destinationFolder}/lastArguments.json` : 'lastArguments.json';
 
   writeFileSync(destination, JSON.stringify(data));
 }
--- tests/configuration.test.ts
+++ tests/configuration.test.ts
@@ -1,4 +1,4 @@
-import { writeSourcePreferences } from '../src/preferences';
+import { writeSourcePreferences, recordArguments } from '../src/preferences';
 import { sources } from '../src/sources';
 import { readFileSync, existsSync, unlinkSync } from 'fs';
 
@@ -18,6 +18,31 @@ describe('the writeSourcePreferences function', () => {
     writeSourcePreferences([sources.BR, sources.MM, sources.DMG], 'tests');
 
     const data = String(readFileSync(testConfigPath));
+    const actual = JSON.parse(data);
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('the recordArguments function', () => {
+  const testArgumentsPath = 'tests/lastArguments.json';
+
+  afterEach(() => {
+    if (existsSync(testArgumentsPath)) {
+      unlinkSync(testArgumentsPath);
+    }
+  });
+
+  it('writes the arguments and spell name used in the invocation of a conjuration function to a json file, which can be parsed', () => {
+    const expected = {
+      sources: [sources.BR, sources.MM, sources.DMG],
+      challengeRating: 1,
+      terrains: ['Land'],
+      spell: 'Conjure Animals'
+    };
+    recordArguments(expected.spell, expected.challengeRating, expected.terrains, expected.sources, 'tests');
+
+    const data = String(readFileSync(testArgumentsPath));
     const actual = JSON.parse(data);
 
     expect(actual).toEqual(expected);
