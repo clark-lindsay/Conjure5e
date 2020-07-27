5ebbf44bae3ac6bc5819587cee0d83a050c81ace
Clark Lindsay <47335328+clark-lindsay@users.noreply.github.com>
2020-07-27 11:52:44 -0700

    Record the last spell used, with its arguments, so that it can be repeated (#8)
    * New function and command to recast the last spell
    writes the arguments and name of the spell used to a config
    file, with an optional path parameter
    
    `npm run recast` will recast the last spell, with an error message if
    there is no spell that has been cast and recorded.
    Some minor changes to the cli, to minimize null checking.
    Moved the `prettyPrintCreature` function to the creature module

--- src/Creature.ts
+++ src/Creature.ts
@@ -1,7 +1,30 @@
+import chalk from 'chalk';
+import boxen from 'boxen';
+
 export interface Creature {
   name: string;
   challengeRating: number;
   terrains: string[];
   type: string;
   source: string;
+}
+
+export function prettyPrintCreatures(creatures: Creature[]): void {
+  let result = '';
+  if (creatures.length == 0) {
+    result +=
+      'No creatures match your parameters.\n Maybe try adding more source books?';
+  }
+  for (const creature of creatures) {
+    let creatureName = '';
+    if (creature.terrains.includes('Air')) {
+      creatureName = chalk.bold(chalk.magenta(creature.name));
+    } else if (creature.terrains.includes('Water')) {
+      creatureName = chalk.bold(chalk.cyan(creature.name));
+    } else {
+      creatureName = chalk.bold(chalk.green(creature.name));
+    }
+    result += `${creatureName}\n\n`;
+  }
+  console.log(boxen(result, { padding: 1, borderColor: 'yellow' }));
 }
