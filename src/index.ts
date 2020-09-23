952304cd82a8a60da508028b487620f235057fa7
clark-lindsay <clarklindsay96@gmail.com>
2020-09-23 14:44:29 -0400

    Initial release to npm

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {
+  conjureAnimals,
+  conjureWoodlandBeings,
+  conjureMinorElementals,
+} from "./conjurationSpells";
+import { cli } from "./cli";
+
+export { conjureAnimals, conjureWoodlandBeings, conjureMinorElementals };
+export { cli };
