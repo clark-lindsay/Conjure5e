41815ced0a15d2b3adda8944136bb2834a22d7cc
clark-lindsay <clarklindsay96@gmail.com>
2020-10-09 11:50:39 -0400

    removed extraneous js files, rebuilt dist

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.conjureMinorElementals = exports.conjureWoodlandBeings = exports.conjureAnimals = void 0;
-var conjurationSpells_1 = require("./conjurationSpells");
-exports.conjureAnimals = conjurationSpells_1.conjureAnimals;
-exports.conjureWoodlandBeings = conjurationSpells_1.conjureWoodlandBeings;
-exports.conjureMinorElementals = conjurationSpells_1.conjureMinorElementals;
